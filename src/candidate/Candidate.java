07fa483a66abbc8c91f71eb1ffd0ad881fea8b91
KarimCharleux <karim.charleux@etu.univ-cotedazur.fr>
2023-09-27 15:13:51 +0200

    Fix repo

---
+++ src/candidate/Candidate.java
@@ -0,0 +1,29 @@
+package candidate;
+
+class Candidate {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public Candidate(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
